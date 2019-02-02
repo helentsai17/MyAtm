776d8d1b717765bacae2213d735ba6daabbfc68d
helentsai <pupu.her@hotmail.com>
2019-02-02 12:46:59 -0500

    Initial commit

---
+++ app/src/main/java/com/home/myatm/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.home.myatm;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private EditText edUserid;
+    private EditText edPasswd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSharedPreferences("atm",MODE_PRIVATE)
+                .edit().putInt("LEVEL",3)
+                .putString("NAME","Tom")
+                .commit();
+        int level=getSharedPreferences("atm",MODE_PRIVATE)
+                .getInt("LEVE",0);
+        Log.d(TAG,"onCreat:"+ level);
+        edUserid= findViewById(R.id.userid);
+        edPasswd= findViewById(R.id.passwd);
+        String userid = getSharedPreferences("atm",MODE_PRIVATE)
+                .getString("USERID","");
+        edUserid.setText(userid);
+
+    }
+    public void login(View view){
+        final String userid = edUserid.getText().toString();
+        final String passwd = edPasswd.getText().toString();
+        FirebaseDatabase.getInstance().getReference("users").child(userid).child("passwd")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String pw = (String) dataSnapshot.getValue();
+                        if(pw.equals(passwd)){
+                            //save userid
+                            getSharedPreferences("atm",MODE_PRIVATE)
+                                    .edit()
+                                    .putString("USERID",userid)
+                                    .apply();
+                            setResult(RESULT_OK);
+                            finish();
+                        }else{
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("登入結果")
+                                    .setMessage("登入失敗")
+                                    .setPositiveButton("OK",null)
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+      //  if ("jack".equals(userid) && "1234".equals(passwd)){
+        //   setResult(RESULT_OK);
+          //   finish();
+        }
+
+    //}
+
+    public void quit(View view){
+
+    }
+}
